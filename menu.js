1f2e262f0cff87f77e0686605f0236bc2b51ab4e
noa-blenkitny <noa.blenkitny@gmail.com>
2022-05-04 14:12:51 +0300

    added menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,27 @@
-let registerBtn = document.querySelector('#registerBtn');
-let loginBtn = document.querySelector('#loginBtn');
-let welocmeDiv = document.querySelector('#welcomeDiv');
-registerBtn.addEventListener('click',()=> 
-{
-    welocmeDiv.display='none'
+var registerBtn
+var loginBtn
+var welocmeDiv
+var registerDiv
+var loginDiv
+$(document).ready(function () {
+    registerBtn = document.getElementById('registerBtn');
+    loginBtn = document.getElementById('loginBtn');
+    welocmeDiv = document.getElementById('welcomeDiv');
+    registerDiv = document.getElementById('registerDiv');
+    loginDiv = document.getElementById('loginDiv');
+
+    registerBtn.onclick = displayRegisterDiv;
+    registerBtn.onclick = displayLoginDiv;
 });
+function displayRegisterDiv() {
+    if (welocmeDiv.style.display !== "none") {
+        welocmeDiv.style.display = "none";
+    }
+    registerDiv.style.display = "block";
+}
+function displayLoginDiv() {
+    if (welocmeDiv.style.display !== "none") {
+        welocmeDiv.style.display = "none";
+    }
+    loginDiv.style.display = "block";
+}
