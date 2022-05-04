65b51bcc82a2eb63c6cff75bb087b8039415e706
noa-blenkitny <noa.blenkitny@gmail.com>
2022-05-04 13:40:55 +0300

    initial commit

---
+++ menu.js
@@ -0,0 +1,7 @@
+let registerBtn = document.querySelector('#registerBtn');
+let loginBtn = document.querySelector('#loginBtn');
+let welocmeDiv = document.querySelector('#welcomeDiv');
+registerBtn.addEventListener('click',()=> 
+{
+    welocmeDiv.display='none'
+});
