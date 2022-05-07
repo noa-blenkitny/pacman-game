6f6aa7082c0ac2229d6043c383669c2f85af0c0d
bargold100 <goldkbar@post.bgu.ac.il>
2022-05-07 21:17:14 +0300

    append cherry and buggy monster

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+//initialize variables
 var context;
 var shape = new Object();
+
+//monsters
+var monster_shape1 = new Object();
+var monster_shape2 = new Object();
+var monster_shape3 = new Object();
+var monster_shape4 = new Object();
+var cherries = new Object();
+
 var board;
 var score;
 var pac_color;
 var start_time;
 var time_elapsed;
-var interval;
 var x;
+var object_befor_cherries=0;
+var object_befor_monster1 = new Object();
+object_befor_monster1.val = 0;
+var flag_if = 0
+
+//intervals
+var interval;
+var cherries_interval;
+var monster1_interval;
+
+//images
+const cherries_img = new Image();
+cherries_img.src = '/images/cherries.png';
+
+const monster1 = new Image();
+monster1.src = '/images/monster1.png';
+
 $(document).ready(function() {
 	context = canvas.getContext("2d");
 	Start();
@@ -35,7 +60,18 @@ function Start() {
 				(i == 6 && j == 2)
 			) {
 				board[i][j] = 4;//wall
-			} else {
+			}
+			else if(i == 4 && j == 4){
+				cherries.i = i;
+				cherries.j = j;
+				board[i][j] = 5;//cherries
+			}
+			else if(i == 0 && j == 0){
+				monster_shape1.i = i;
+				monster_shape1.j = j;
+				board[i][j] = 3;//monster
+			}
+			else {
 				var randomNum = Math.random();
 				if (randomNum <= (1.0 * food_remain) / cnt) {
 					food_remain--;
@@ -74,7 +110,17 @@ function Start() {
 		false
 	);
 	interval = setInterval(UpdatePosition, 145);
+	cherries_interval = setInterval(updateCherriesPosition, 500);
+	monster1_interval = setInterval(updateMonster1Position, 500 ,monster_shape1, object_befor_monster1);
+
+	
 }//end start
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+  }
 
 function findRandomEmptyCell(board) {
 	var i = Math.floor(Math.random() * 9 + 1);
@@ -168,8 +214,15 @@ function Draw() {//draw canvas
 				context.fillStyle = "grey"; //color
 				context.fill();
 			}
+			else if(board[i][j]==5){
+				context.drawImage(cherries_img,center.x-30, center.y-30,cherries_img.width/15,cherries_img.height/15);
+			}
+			else if(board[i][j]==3){
+				context.drawImage(monster1,center.x-30, center.y-30,monster1.width/15,monster1.height/15);
+			}
 		}
 	}
+
 }
 
 function UpdatePosition() {
@@ -198,17 +251,106 @@ function UpdatePosition() {
 	if (board[shape.i][shape.j] == 1) {
 		score++;
 	}
+	if(board[shape.i][shape.j] == 5){
+		score += 50;
+		window.clearInterval(cherries_interval);
+	}
+	if(board[shape.i][shape.j] == 3){
+		score -= 50;
+		shape.i, shape.j = findRandomEmptyCell(board)
+
+	}
 	board[shape.i][shape.j] = 2;//update packman position
 	var currentTime = new Date();
 	time_elapsed = (currentTime - start_time) / 1000;
 	if (score >= 20 && time_elapsed <= 10) {
 		pac_color = "green";
 	}
-	if (score == 50) {
+	if (score >= 50) {
+		Draw();
 		window.clearInterval(interval);
 		window.alert("Game completed");
 	}
 	 else {
 		Draw();
 	}
+}
+
+function updateCherriesPosition(){
+	cherries_position = getRandomInt(1, 5)
+	if (cherries_position == 1) {
+		if (cherries.j > 0 && board[cherries.i][cherries.j - 1] != 4) {
+			board[cherries.i][cherries.j] = object_befor_cherries
+			object_befor_cherries = board[cherries.i][cherries.j - 1];
+			cherries.j--;
+			
+		}
+	}
+	else if (cherries_position == 2) {
+		if (cherries.j < 9 && board[cherries.i][cherries.j + 1] != 4) {
+			board[cherries.i][cherries.j] = object_befor_cherries
+			object_befor_cherries = board[cherries.i][cherries.j + 1];
+			cherries.j++;
+		}
+	}
+	else if (cherries_position == 3) {
+		if (cherries.i > 0 && board[cherries.i - 1][cherries.j] != 4) {
+			board[cherries.i][cherries.j] = object_befor_cherries
+			object_befor_cherries = board[cherries.i-1][cherries.j];
+			cherries.i--;
+		}
+	}
+	else if (cherries_position == 4) {
+		if (cherries.i < 9 && board[cherries.i + 1][cherries.j] != 4) {
+			board[cherries.i][cherries.j] = object_befor_cherries
+			object_befor_cherries = board[cherries.i+1][cherries.j];
+			cherries.i++;
+		}
+	}
+	board[cherries.i][cherries.j] = 5;//update cherries position
+}
+
+function updateMonster1Position(monster, object_befor_monster){
+	let monster_direction = getRandomInt(1, 3);
+	if (monster_direction == 1){
+
+	if (shape.j-monster.j <= 0) {
+		if (monster.j > 0 && board[monster.i][monster.j - 1] != 4) {
+			board[monster.i][monster.j] = object_befor_monster.val
+			object_befor_monster.val = board[monster.i][monster.j - 1];
+			monster.j--;
+			flag_if = 1;
+		}
+	}
+	else {
+		if (monster.j < 9 && board[monster.i][monster.j + 1] != 4) {
+			board[monster.i][monster.j] = object_befor_monster.val
+			object_befor_monster.val = board[monster.i][monster.j + 1];
+			monster.j++;
+			flag_if = 1;
+		}
+	}
+}
+else{
+	if (shape.i - monster.i <= 0) {
+		if (monster.i > 0 && board[monster.i - 1][monster.j] != 4) {
+			board[monster.i][monster.j] = object_befor_monster.val
+			object_befor_monster.val = board[monster.i-1][monster.j];
+			monster.i--;
+			flag_if = 1;
+		}
+	}
+	else {
+		if (monster.i < 9 && board[monster.i + 1][monster.j] != 4) {
+			board[monster.i][monster.j] = object_befor_monster.val
+			object_befor_monster.val = board[monster.i+1][monster.j];
+			monster.i++;
+			flag_if = 1;
+		}
+	}
+}
+	if (flag_if ==  1){
+	board[monster.i][monster.j] = 3;//update monster position
+	}
+	flag_if = 0;
 }
